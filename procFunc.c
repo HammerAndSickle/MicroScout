a52b959915621dc6ac8bd423eeb375b6cf5f2984
user <user@localhost.localdomain>
2015-02-27 19:04:18 +0900

    예외 수정, 변수 자료구조 변경

--- procFunc.c
+++ procFunc.c
@@ -14,9 +14,17 @@
 #define USE_SOFT_LIMIT 0
 #define USE_HARD_LIMIT 1	//리소스 제한값 함수의 옵션으로 사용할 상수.
 
+#define SECTOR1 0		//부문별로 점수 인덱스를 상수로 정의한 것.
+#define SECTOR2 1
+#define SECTOR3 2
+#define SECTOR4 3
+#define SECTOR5 4
+
+#define NO_VALUE -1		//프로세스의 몇몇 정보를 얻지 못하면 -1을 저장하게 된다.
+
 //Proc의 정보들을 저장하는 데이터구조다.
 //값을 찾을 수 없을 경우 -1이 저장되도록 하였다.
-//테스트 결과, VSZ, RSS에만 -1이 저장되는 것을 확인, 값을 찾지 못하는 경우를 VSZ, RSS에만 가정한다.
+//테스트 결과, CPUusage, VSZ, RSS에만 -1이 저장되는 것을 확인, 값을 찾지 못하는 경우를 CPUusage, VSZ, RSS에만 가정한다.
 typedef struct ProcStats
 {
 	pid_t pid;		//PID
@@ -30,47 +38,45 @@ typedef struct ProcStats
 	int niceVal;		//우선순위 값
 
 	//각 부문별로 중요도 점수 저장
-	float score1;
-	float score2;
-	float score3;
-	float score4;
-	float score5;
+	float scores[PROC_CATEGORY];
 	
 } ProcStats;
 
+//선언된 함수들. eval계열의 함수에 있는 매개변수인 sector는 주어진 값에 따라 score의 인덱스로 쓰인다.
+
 void getProcScores();
 int getProcStats(ProcStats* procs);
-void eval1_nice(ProcStats* procs, int num);
-void eval2_FD(ProcStats* procs, int num, int option);
-void eval3_CPU_USAGE(ProcStats* procs, int num);
-void eval4_pids_comp(ProcStats* procs, int num);
-void eval5_memory(ProcStats* procs, int num);
+void eval1_nice(ProcStats* procs, int num, int sector);
+void eval2_FD(ProcStats* procs, int num, int option, int sector);
+void eval3_CPU_USAGE(ProcStats* procs, int num, int sector);
+void eval4_pids_comp(ProcStats* procs, int num, int sector);
+void eval5_memory(ProcStats* procs, int num, int sector);
 
 
 //임시 dat 파일에 기록된 프로세스 정보들을 사용해 중요도 값을 계산한다.
 void getProcScores()
 {
 	int count; 					//얻어낸 프로세스의 총 개수를 저장한다.
-	//int idx;
+	int idx;
 
 	ProcStats* procs = (ProcStats*)malloc(sizeof(ProcStats)*MAXIMUM_PROCS); //프로세스의 정보들이 담기는 구조체 배열
 
 	//아래 메소드에서 프로세스 정보를 구조체 배열에 기록한다.
 	count = getProcStats(procs);
 	
-	eval1_nice(procs, count);			//nice 값을 이용해 점수 계산
-	eval2_FD(procs, count, USE_HARD_LIMIT);		//FDSize 값을 이용해 점수 계산
-	eval3_CPU_USAGE(procs, count);			//CPU 사용률을 이용해 점수 계산
-	eval4_pids_comp(procs, count);			//PID 값들을 이용해 점수 계산
-	eval5_memory(procs, count);			//memory 관련 값들을 이용해 점수 계산
+	eval1_nice(procs, count, SECTOR1);			//nice 값을 이용해 점수 계산
+	eval2_FD(procs, count, USE_HARD_LIMIT, SECTOR2);	//FDSize 값을 이용해 점수 계산
+	eval3_CPU_USAGE(procs, count, SECTOR3);			//CPU 사용률을 이용해 점수 계산
+	eval4_pids_comp(procs, count, SECTOR4);			//PID 값들을 이용해 점수 계산
+	eval5_memory(procs, count, SECTOR5);			//memory 관련 값들을 이용해 점수 계산
 	
 
 
 	//-----------------------테스트용 출력 코드
-	/*for(idx = 0; idx < count; idx++)
+	for(idx = 0; idx < count; idx++)
 	{
-		printf("%d : %f %f %f %f %f\n", (procs + idx)->pid, (procs + idx)->score1, (procs + idx)->score2, (procs + idx)->score3, (procs + idx)->score4, (procs + idx)->score5);
-	}*/
+		printf("%d : %f %f %f %f %f\n", (procs + idx)->pid, (procs + idx)->scores[0], (procs + idx)->scores[1], (procs + idx)->scores[2], (procs + idx)->scores[3], (procs + idx)->scores[4]);
+	}
 
 	free(procs);
 
@@ -98,7 +104,7 @@ int getProcStats(ProcStats* procs)
 }
 
 //nice 값을 중요도로 환산한다.
-void eval1_nice(ProcStats* procs, int num)
+void eval1_nice(ProcStats* procs, int num, int sector)
 {
 	int idx;
 	int val;
@@ -106,14 +112,14 @@ void eval1_nice(ProcStats* procs, int num)
 	for(idx = 0; idx < num; idx++)
 	{
 		val = -(((procs + idx)->niceVal) -19);	//-19 후 *(-1)
-		(procs + idx)->score1 = ((float)(val)/40)*100;	//val 값을 float화하여 (100/40)을 곱한다.
+		(procs + idx)->scores[sector] = ((float)(val)/40)*100;	//val 값을 float화하여 (100/40)을 곱한다.
 	}
 
 	//ex) nice가 0일 경우, 우선도 값이 19이므로, 19 * (100/40)은 47.5 가 된다.
 }
 
 //FDSize 값을 중요도로 환산한다.
-void eval2_FD(ProcStats* procs, int num, int option)
+void eval2_FD(ProcStats* procs, int num, int option, int sector)
 {
 	struct rlimit rlim;			//리소스 제한값을 얻기 위한 구조체
 	unsigned long max;			//maximum limit value
@@ -139,27 +145,31 @@ void eval2_FD(ProcStats* procs, int num, int option)
 	
 	for(idx = 0; idx < num; idx++)
 	{
-		(procs + idx)->score2 = (((float)((procs + idx)->FDSize))/max)*100;	//float으로 나타내도록 한다.
+		(procs + idx)->scores[sector] = (((float)((procs + idx)->FDSize))/max)*100;	//float으로 나타내도록 한다.
 	}
 	
 
 }
 
 //CPU 사용률을 점수로 환산한다.
-void eval3_CPU_USAGE(ProcStats* procs, int num)
+void eval3_CPU_USAGE(ProcStats* procs, int num, int sector)
 {
 	int idx;
 	
 	for(idx = 0; idx < num; idx++)
 	{
-		(procs + idx)->score3 = (procs + idx)->CPUusage;
+		//값을 찾지 못해 -1이 저장되어있을 경우,
+		if( (procs + idx)->CPUusage == NO_VALUE )
+			(procs + idx)->scores[sector] = 0.0;
+		else
+			(procs + idx)->scores[sector] = (procs + idx)->CPUusage;
 	}
 
 
 }
 
 //PID들을 비교하여 중요도를 조사해 본다.
-void eval4_pids_comp(ProcStats* procs, int num)
+void eval4_pids_comp(ProcStats* procs, int num, int sector)
 {
 	int idx;
 	float score;
@@ -180,12 +190,12 @@ void eval4_pids_comp(ProcStats* procs, int num)
 		if( (procs + idx)->ppid == 0 || (procs + idx)->ppid == 1 )
 			score += 10.0;
 
-		(procs + idx)->score4 = score;
+		(procs + idx)->scores[sector] = score;
 	}
 }
 
 //memory VSZ, RSS값들을 이용해 중요도를 조사해 본다.
-void eval5_memory(ProcStats* procs, int num)
+void eval5_memory(ProcStats* procs, int num, int sector)
 {
 	int idx;
 
@@ -196,9 +206,9 @@ void eval5_memory(ProcStats* procs, int num)
 	{
 		//VSZ나 RSS가-1일 경우는 20점을 부여한다.
 		if( (procs + idx)->VSZ == -1 || (procs + idx)->RSS == -1 )
-			(procs + idx)->score5 = 20.0;
+			(procs + idx)->scores[sector] = 20.0;
 
 		//그 외의 경우, RSS/VSZ값을 효율 점수로 지정.
-		else (procs + idx)->score5 = ((float)((procs + idx)->RSS) / (float)((procs + idx)->VSZ)) * 100;
+		else (procs + idx)->scores[sector] = ((float)((procs + idx)->RSS) / (float)((procs + idx)->VSZ)) * 100;
 	}
 }
